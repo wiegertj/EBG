c00de95ac45105bf98270db8c2a5cb7c64dc2de0
juliusw <uimmv@student.kit.edu>
2023-12-19 12:24:28 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ebg",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     entry_points={
         'console_scripts': ['ebg = EBG.__main__:main']
